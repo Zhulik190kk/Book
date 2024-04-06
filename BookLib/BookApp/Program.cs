059eb180283956cc0cd5ac480fc167ac2f1f690f
Zhulik190kk <151401604+Zhulik190kk@users.noreply.github.com>
2024-04-07 01:55:07 +0300

    Add files via upload

---
+++ BookLib/BookApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using BookLibrary;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть кількість книг: ");
+            int n = int.Parse(Console.ReadLine());
+
+            ExtendedBook[] books = new ExtendedBook[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Введіть дані для книги {i + 1}:");
+                Console.Write("Назва: ");
+                string title = Console.ReadLine();
+                Console.Write("Автор: ");
+                string author = Console.ReadLine();
+                Console.Write("Видавництво: ");
+                string publisher = Console.ReadLine();
+                Console.Write("Рік публікації: ");
+                int yearPublished = int.Parse(Console.ReadLine());
+                Console.Write("Кількість сторінок: ");
+                int pages = int.Parse(Console.ReadLine());
+                Console.Write("Ціна(у $): ");
+                double price = double.Parse(Console.ReadLine());
+                Console.Write("Доступна? (так/ні): ");
+                bool isAvailable = Console.ReadLine().ToLower() == "так";
+                Console.Write("Жанр: ");
+                string genre = Console.ReadLine();
+
+                books[i] = new ExtendedBook(title, author, publisher, yearPublished, pages, price, isAvailable, genre);
+            }
+
+            Console.WriteLine("\nІнформація про книги:");
+            foreach (var book in books)
+            {
+                Console.WriteLine();
+                book.PrintBookInfo();
+            }
+        }
+    }
+}
---
+++ BookLib/BookLib/ClassesBook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace BookLibrary
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public int YearPublished { get; set; }
+        public int Pages { get; set; }
+        public double Price { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public Book(string title, string author, string publisher, int yearPublished, int pages, double price, bool isAvailable)
+        {
+            Title = title;
+            Author = author;
+            Publisher = publisher;
+            YearPublished = yearPublished;
+            Pages = pages;
+            Price = price;
+            IsAvailable = isAvailable;
+        }
+
+        public void PrintBookInfo()
+        {
+            Console.WriteLine("Назва: " + Title);
+            Console.WriteLine("Автор: " + Author);
+            Console.WriteLine("Видавництво: " + Publisher);
+            Console.WriteLine("Рік публікації: " + YearPublished);
+            Console.WriteLine("Кількість сторінок: " + Pages);
+            Console.WriteLine("Ціна: $" + Price);
+            Console.WriteLine("Доступна: " + (IsAvailable ? "так" : "ні"));
+        }
+    }
+
+    public class ExtendedBook : Book
+    {
+        public string Genre { get; set; }
+
+        public ExtendedBook(string title, string author, string publisher, int yearPublished, int pages, double price, bool isAvailable, string genre)
+            : base(title, author, publisher, yearPublished, pages, price, isAvailable)
+        {
+            Genre = genre;
+        }
+    }
+}
